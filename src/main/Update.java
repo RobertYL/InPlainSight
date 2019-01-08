d5343708d6325edb26ba2b95f18ebd621ba48b83
austin-li <austinmingli@gmail.com>
2019-01-08 12:08:38 -0800

    update ruffian positions with input

--- src/main/Update.java
+++ src/main/Update.java
@@ -1,5 +1,9 @@
 package main;
 
 public class Update {
-
+	public static void update(int[] input) {
+		for (int i = 0; i < Settings.RUFFIAN_NUM; i++) {
+			MainController.ruffians[i].update(input[i]);
+		}
+	}
 }
