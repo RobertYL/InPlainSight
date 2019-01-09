c273c4b0ef0305a8ad2d0361cb687933bee951e3
austin-li <austinmingli@gmail.com>
2019-01-09 10:23:15 -0800

    added double tap detection

--- src/input/Input.java
+++ src/input/Input.java
@@ -6,14 +6,35 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class Input implements KeyListener, MouseListener {
+	int[] currentInput = new int[3];
 	int[] input = new int[3];
+	int[] ticks = new int[3];
+	boolean[] dTap = new boolean[3];
 	
 	public Input() {
 		
 	}
 	
+	public void tick() {
+		for (int i = 0; i < input.length; i++) {
+			dTap[i] = false;
+			ticks[i]--;
+			if (currentInput[i] != 0) {
+				if (currentInput[i] == input[i]) {
+					if (ticks[i] > 0) dTap[i] = true;
+					ticks[i] = 20;
+				}
+				input[i] = currentInput[i];
+			}
+			currentInput[i] = 0;
+		}
+	}
+	
 	public int[] getInput() {
 		return input;
+	}
+	public boolean[] getDTap() {
+		return dTap;
 	}
 	
 	@Override
@@ -43,40 +64,40 @@ public class Input implements KeyListener, MouseListener {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		int keyCode = e.getKeyCode();
-		if (keyCode == 27) System.exit(1);
-
-		if(keyCode == 87)
-			input[0] = 1;
-		else if(keyCode == 68)
-			input[0] = 2;
-		else if(keyCode == 83)
-			input[0] = 3;
-		else if(keyCode == 65)
-			input[0] = 4;
 		
-		else if(keyCode == 73)
-			input[1] = 1;
-		else if(keyCode == 76)
-			input[1] = 2;
-		else if(keyCode == 75)
-			input[1] = 3;
-		else if(keyCode == 74)
-			input[1] = 4;
-		
-		else if(keyCode == 38)
-			input[2] = 1;
-		else if(keyCode == 39)
-			input[2] = 2;
-		else if(keyCode == 40)
-			input[2] = 3;
-		else if(keyCode == 37)
-			input[2] = 4;
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		if (keyCode == 27) System.exit(1);
 
+		if(keyCode == 87)
+			currentInput[0] = 1;
+		else if(keyCode == 68)
+			currentInput[0] = 2;
+		else if(keyCode == 83)
+			currentInput[0] = 3;
+		else if(keyCode == 65)
+			currentInput[0] = 4;
+		
+		else if(keyCode == 73)
+			currentInput[1] = 1;
+		else if(keyCode == 76)
+			currentInput[1] = 2;
+		else if(keyCode == 75)
+			currentInput[1] = 3;
+		else if(keyCode == 74)
+			currentInput[1] = 4;
+		
+		else if(keyCode == 38)
+			currentInput[2] = 1;
+		else if(keyCode == 39)
+			currentInput[2] = 2;
+		else if(keyCode == 40)
+			currentInput[2] = 3;
+		else if(keyCode == 37)
+			currentInput[2] = 4;
 	}
 
 	@Override
