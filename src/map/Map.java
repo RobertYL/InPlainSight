e4016b1b2e584089d75952f39841173209495d47
RobertYL <rob3rtyl33@gmail.com>
2019-01-10 09:08:08 -0800

    Added direct access to all coordinates

--- src/map/Map.java
+++ src/map/Map.java
@@ -1,5 +1,6 @@
 package map;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import main.Settings;
@@ -15,14 +16,42 @@ public class Map {
 		// 6 Fabric Stockpile
 		// 7 Barrel
 		// 8 Dyed Fabric Stockpile
-	
+	public ArrayList<Pair<Integer, Integer>> wheel = new ArrayList<Pair<Integer, Integer>>();
+	public ArrayList<Pair<Integer, Integer>> loom = new ArrayList<Pair<Integer, Integer>>();
+	public ArrayList<Pair<Integer, Integer>> barrel = new ArrayList<Pair<Integer, Integer>>();
+	public Pair<Integer, Integer> woolStockpile, yarnStockpile, fabricStockpile, dyedFabricStockpile;
 	
 	public Map() {
+		
+		
 		Scanner sc = new Scanner("src/map/default.map");
 		for(int i = 0; i < Settings.ROW_NUM; i++) {
 			for(int j = 0; j < Settings.COL_NUM; j++) {
 				map[j][i] = sc.nextInt();
+				if(map[j][i] == 2 && !(woolStockpile instanceof Pair))
+					woolStockpile = new Pair<Integer, Integer>(j, i);
+				if(map[j][i] == 3)
+					wheel.add(new Pair<Integer, Integer>(j, i));
+				if(map[j][i] == 4 && !(yarnStockpile instanceof Pair))
+					yarnStockpile = new Pair<Integer, Integer>(j, i);
+				if(map[j][i] == 5)
+					loom.add(new Pair<Integer, Integer>(j, i));
+				if(map[j][i] == 6 && !(fabricStockpile instanceof Pair))
+					fabricStockpile = new Pair<Integer, Integer>(j, i);
+				if(map[j][i] == 7)
+					barrel.add(new Pair<Integer, Integer>(j, i));
+				if(map[j][i] == 8 && !(dyedFabricStockpile instanceof Pair))
+					dyedFabricStockpile = new Pair<Integer, Integer>(j, i);
 			}
 		}
+	}
+}
+
+class Pair<A, B> {
+	public final A first;
+	public final B second;
+	public Pair(final A first, final B second) {
+		this.first = first;
+		this.second = second;
 	}
 }
