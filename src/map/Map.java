e75f87bbf8234b49ddbe9e10f5825a89380841d4
austin-li <austinmingli@gmail.com>
2019-01-10 09:24:35 -0800

    added debugging for map and objects on map

--- src/map/Map.java
+++ src/map/Map.java
@@ -18,9 +18,9 @@ public class Map {
 		// 6 Fabric Stockpile
 		// 7 Barrel
 		// 8 Dyed Fabric Stockpile
-	public ArrayList<Pair<Integer, Integer>> wheel = new ArrayList<Pair<Integer, Integer>>();
-	public ArrayList<Pair<Integer, Integer>> loom = new ArrayList<Pair<Integer, Integer>>();
-	public ArrayList<Pair<Integer, Integer>> barrel = new ArrayList<Pair<Integer, Integer>>();
+	public ArrayList<Pair<Integer, Integer>> wheels = new ArrayList<Pair<Integer, Integer>>();
+	public ArrayList<Pair<Integer, Integer>> looms = new ArrayList<Pair<Integer, Integer>>();
+	public ArrayList<Pair<Integer, Integer>> barrels = new ArrayList<Pair<Integer, Integer>>();
 	public Pair<Integer, Integer> woolStockpile, yarnStockpile, fabricStockpile, dyedFabricStockpile;
 	
 	public Map() {
@@ -33,22 +33,41 @@ public class Map {
 		}
 		for(int i = 0; i < Settings.ROW_NUM; i++) {
 			for(int j = 0; j < Settings.COL_NUM; j++) {
-				System.out.println("d");
 				map[j][i] = sc.nextInt();
 				if(map[j][i] == 2 && !(woolStockpile instanceof Pair))
 					woolStockpile = new Pair<Integer, Integer>(j, i);
 				if(map[j][i] == 3)
-					wheel.add(new Pair<Integer, Integer>(j, i));
+					wheels.add(new Pair<Integer, Integer>(j, i));
 				if(map[j][i] == 4 && !(yarnStockpile instanceof Pair))
 					yarnStockpile = new Pair<Integer, Integer>(j, i);
 				if(map[j][i] == 5)
-					loom.add(new Pair<Integer, Integer>(j, i));
+					looms.add(new Pair<Integer, Integer>(j, i));
 				if(map[j][i] == 6 && !(fabricStockpile instanceof Pair))
 					fabricStockpile = new Pair<Integer, Integer>(j, i);
 				if(map[j][i] == 7)
-					barrel.add(new Pair<Integer, Integer>(j, i));
+					barrels.add(new Pair<Integer, Integer>(j, i));
 				if(map[j][i] == 8 && !(dyedFabricStockpile instanceof Pair))
 					dyedFabricStockpile = new Pair<Integer, Integer>(j, i);
+			}
+		}
+		if (Settings.DEBUG) {
+			for (int i = 0; i < Settings.ROW_NUM; i++) {
+				for (int j = 0; j < Settings.COL_NUM; j++) {
+					System.out.print(map[j][i] + " ");
+				}
+				System.out.println();
+			}
+			System.out.println("WHEELS");
+			for (Pair<Integer, Integer> pair : wheels) {
+				System.out.println(pair.first + " " + pair.second);
+			}
+			System.out.println("LOOMS");
+			for (Pair<Integer, Integer> pair : looms) {
+				System.out.println(pair.first + " " + pair.second);
+			}
+			System.out.println("BARRELS");
+			for (Pair<Integer, Integer> pair : barrels) {
+				System.out.println(pair.first + " " + pair.second);
 			}
 		}
 	}
