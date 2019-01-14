d142a8995097c971083e0843039e96335f5e4aad
austin-li <austinmingli@gmail.com>
2019-01-14 12:50:12 -0800

    added work animation

--- src/entity/machine/Loom.java
+++ src/entity/machine/Loom.java
@@ -2,31 +2,50 @@ package entity.machine;
 
 import java.awt.Graphics;
 
-import javax.swing.ImageIcon;
-
 import entity.machine.Machine;
 import main.Settings;
 
 public class Loom extends Machine {
 	public Loom(int width, int height, int x, int y, int job) {
 		super(width, height, x, y, job);
-		ImageIcon icon = new ImageIcon(Settings.LOOM_PATH);
-		image = icon.getImage();
-		image = sheet.getSubimage(0, 0, 10, 10);
+		image = sheet.getSubimage(820, 392, 64, 80);
+		frameX = 420;
+		frameY = 788;
 	}
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(image, x, y, null);
+		if (frames < 0) {
+			g.drawImage(image, x, y, null);
+		} else {
+			g.drawImage(image, x, y - 48, null);
+		}
 	}
 	
 	@Override
 	public void tick() {
 		frames--;
-		image = sheet.getSubimage(0,  0, 10, 10);
+		if (frames < 0) {
+			image = sheet.getSubimage(820, 392, 64, 80);
+			return;
+		}
+		image = sheet.getSubimage(frameX, frameY, 64, 128);
+		if (frames % 4 == 0) {
+			if (frames % 12 == 0) {
+				frameX = 420;
+				frameY = 788;
+			} else {
+				frameX = 420;
+				frameY += 128 + 4;
+			}
+		} else {
+			frameX += 64 + 4;
+		}
 	}
 	
+	@Override
 	public void work() {
+		if (frames > 0) return;
 		this.frames = Settings.LOOM_FRAMES;
 	}
 }
